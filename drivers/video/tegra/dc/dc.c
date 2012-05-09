c1641595687e0962317542db6f763534d61a4044
Pritesh Raithatha <praithatha@nvidia.com>
2012-05-09 19:36:46 +0530

    video: tegra: dc: correct dc initialisation sequence
    -Move _tegra_dc_enable to before irq_request and remove
     disable_dc_irq.
    -It will remove warning of "IRQ when DC not powered!".
    
    Bug 955184
    
    Change-Id: If9b039f3f1635d92f10bfc54af08101972fc3d57
    Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-on: http://git-master/r/101498
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -3148,6 +3148,11 @@ static int tegra_dc_probe(struct nvhost_device *ndev)
 		dc->ext = NULL;
 	}
 
+	mutex_lock(&dc->lock);
+	if (dc->pdata->flags & TEGRA_DC_FLAG_ENABLED)
+		dc->enabled = _tegra_dc_enable(dc);
+	mutex_unlock(&dc->lock);
+
 	/* interrupt handler must be registered before tegra_fb_register() */
 	if (request_irq(irq, tegra_dc_irq, IRQF_DISABLED,
 			dev_name(&ndev->dev), dc)) {
@@ -3155,14 +3160,6 @@ static int tegra_dc_probe(struct nvhost_device *ndev)
 		ret = -EBUSY;
 		goto err_put_min_emc_clk;
 	}
-
-	/* hack to balance enable_irq calls in _tegra_dc_enable() */
-	disable_dc_irq(dc->irq);
-
-	mutex_lock(&dc->lock);
-	if (dc->pdata->flags & TEGRA_DC_FLAG_ENABLED)
-		dc->enabled = _tegra_dc_enable(dc);
-	mutex_unlock(&dc->lock);
 
 	tegra_dc_create_debugfs(dc);
 
